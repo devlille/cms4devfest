a81ea6cfa40b34f5228454be91904156bf42e492
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-07-07 12:17:48 +0200

    feat(partner): Add Phone and Emails inputs

--- src/i18n/fr.js
+++ src/i18n/fr.js
@@ -1,193 +1,197 @@
 export default {
   APP: {
-    NAME: 'CMS4DevFest',
-    COMPANY: 'a GDG Lille project',
+    NAME: "CMS4DevFest",
+    COMPANY: "a GDG Lille project"
   },
   ACTIONS: {
-    CLOSE: 'Fermer',
-    CANCEL: 'Annuler',
-    VALID: 'Valider',
-    CONTINUE: 'Continuer',
-    RETRY: 'Réessayer',
-    MODIFY: 'Modifier',
-    DELETE: 'Supprimer',
-    SEE: 'Voir',
-    IS_LOADING: 'Chargement en cours ...',
+    CLOSE: "Fermer",
+    CANCEL: "Annuler",
+    VALID: "Valider",
+    CONTINUE: "Continuer",
+    RETRY: "Réessayer",
+    MODIFY: "Modifier",
+    DELETE: "Supprimer",
+    SEE: "Voir",
+    IS_LOADING: "Chargement en cours ..."
   },
   APP_USER_MENU: {
-    SIGN_OUT: 'Se déconnecter',
+    SIGN_OUT: "Se déconnecter"
   },
   APP_MORE_MENU: {
     CODE: {
-      LABEL: 'Le projet',
-      HOMEPAGE: 'Fork me on Github',
-      FEAT_REQUEST: 'Proposer une fonctionnalité',
-      BUG_REQUEST: 'Remonter un bug',
+      LABEL: "Le projet",
+      HOMEPAGE: "Fork me on Github",
+      FEAT_REQUEST: "Proposer une fonctionnalité",
+      BUG_REQUEST: "Remonter un bug"
     },
     SOCIAL: {
-      LABEL: 'Nous suivre',
-      TWITTER: 'Twitter',
-      LINKEDIN: 'LinkedIn',
-    },
+      LABEL: "Nous suivre",
+      TWITTER: "Twitter",
+      LINKEDIN: "LinkedIn"
+    }
   },
   APP_DEVFEST: {
-    DDAY: 'J',
-    DEVFEST: 'DevFest',
+    DDAY: "J",
+    DEVFEST: "DevFest"
   },
   APP_FILE_UPLOADER: {
-    LABEL: 'Cliquer pour uploader une image',
+    LABEL: "Cliquer pour uploader une image"
   },
   EDITIONS: {
-    LABEL: 'Aucune édition | Votre édition | Vos éditions',
-    ERROR: 'Echec lors de la récupération de vos éditions.',
+    LABEL: "Aucune édition | Votre édition | Vos éditions",
+    ERROR: "Echec lors de la récupération de vos éditions."
   },
   EDITIONS_EDIT: {
     LABEL: {
-      CREATE: 'Créer une édition',
-      UPDATE: 'Modifier une édition',
+      CREATE: "Créer une édition",
+      UPDATE: "Modifier une édition"
     },
-    ERROR: 'Echec lors de la sauvegarde.',
-    INFO: 'Tous les champs sont obligatoires.',
+    ERROR: "Echec lors de la sauvegarde.",
+    INFO: "Tous les champs sont obligatoires."
   },
   EDITIONS_DASHBOARD: {
-    LABEL: 'Vue d\'ensemble',
+    LABEL: "Vue d'ensemble",
     PARTNERS: {
-      LABEL: '{0} partenaire | {0} partenaire | {0} partenaires',
-      CREATE: 'Créer un partenaire',
+      LABEL: "{0} partenaire | {0} partenaire | {0} partenaires",
+      CREATE: "Créer un partenaire"
     },
     SPEAKERS: {
-      LABEL: '{0} speaker | {0} speaker | {0} speakers',
-      CREATE: 'Créer un speaker',
+      LABEL: "{0} speaker | {0} speaker | {0} speakers",
+      CREATE: "Créer un speaker"
     },
     TALKS: {
-      LABEL: '{0} talk | {0} talk | {0} talks',
-      CREATE: 'Créer un talk',
+      LABEL: "{0} talk | {0} talk | {0} talks",
+      CREATE: "Créer un talk"
     },
-    ERROR: 'Echec lors de la récupération de vos éditions.',
+    ERROR: "Echec lors de la récupération de vos éditions."
   },
   EDITION: {
     NAME: {
-      LABEL: 'Nom',
-      EXAMPLE: 'e.g. DevFest Lille 2019',
+      LABEL: "Nom",
+      EXAMPLE: "e.g. DevFest Lille 2019"
     },
     URL: {
-      LABEL: 'URL',
-      EXAMPLE: 'e.g. https://devfest.gdglille.org',
+      LABEL: "URL",
+      EXAMPLE: "e.g. https://devfest.gdglille.org"
     },
     CONFERENCE_HALL: {
-      LABEL: 'Conference Hall',
+      LABEL: "Conference Hall",
       EVENT: {
-        LABEL: 'ID de l\'évènement',
-        EXAMPLE: 'e.g. uzZ3ekhBTqMW4ejLDmy4',
+        LABEL: "ID de l'évènement",
+        EXAMPLE: "e.g. uzZ3ekhBTqMW4ejLDmy4"
       },
       API_KEY: {
-        LABEL: 'Api Key',
-        EXAMPLE: 'e.g. d773çfyrh-9dzd5-4dzd5-a5bf-b8dzdzdzddf',
-      },
-    },
+        LABEL: "Api Key",
+        EXAMPLE: "e.g. d773çfyrh-9dzd5-4dzd5-a5bf-b8dzdzdzddf"
+      }
+    }
   },
   PARTNERS_EDIT: {
     LABEL: {
-      CREATE: 'Créer un partenaire',
-      UPDATE: 'Modifier un partenaire',
+      CREATE: "Créer un partenaire",
+      UPDATE: "Modifier un partenaire"
     },
     ERRORS: {
-      REQUIRED: 'Obligatoire',
-      SAVING: 'Echec lors de la sauvegarde.',
+      REQUIRED: "Obligatoire",
+      SAVING: "Echec lors de la sauvegarde."
     },
-    INFOS: 'Informations générales',
-    ADMINISTRATIVE: 'Informations administratives',
-    CONTACT: 'Contact',
-    OPTIONS: 'Options',
+    INFOS: "Informations générales",
+    ADMINISTRATIVE: "Informations administratives",
+    CONTACT: "Contact",
+    OPTIONS: "Options"
   },
   PARTNER: {
-    NAME: 'Nom',
-    SIRET: 'Numéro SIRET',
+    NAME: "Nom",
+    SIRET: "Numéro SIRET",
     ADDRESS: {
-      ROAD: 'Rue, voie',
-      ZIP_CODE: 'Code postal',
-      TOWN: 'Ville',
+      ROAD: "Rue, voie",
+      ZIP_CODE: "Code postal",
+      TOWN: "Ville"
     },
     CONTACT: {
-      NAME: 'Prénom & NOM',
-      FUNCTION: 'Rôle',
+      NAME: "Prénom & NOM (contact principal)",
+      FUNCTION: "Rôle (contact principal)",
+      PHONE: "Téléphone (contact principal)",
+      EMAILS: "Emails (personnes actives dans ce partenariat)"
     },
-    URL: 'Url',
-    LOGO: 'Logo',
-    ACTIVE_ON: 'Date d\'activation',
-    LEVEL: 'Niveau',
+    URL: "Url",
+    LOGO: "Logo",
+    ACTIVE_ON: "Date d'activation",
+    LEVEL: "Niveau"
   },
   PACK: {
-    GOLD: 'Gold',
-    SILVER: 'Silver',
-    BRONZE: 'Bronze',
-    CONTRIBUTEUR: 'Contributeur',
+    GOLD: "Gold",
+    SILVER: "Silver",
+    BRONZE: "Bronze",
+    CONTRIBUTEUR: "Contributeur"
   },
   SPEAKERS_EDIT: {
     LABEL: {
-      CREATE: 'Créer un speaker',
-      UPDATE: 'Modifier un speaker',
+      CREATE: "Créer un speaker",
+      UPDATE: "Modifier un speaker"
     },
     CONFERENCE_HALL: {
-      LOADING: 'Chargement des données depuis ConferenceHall, merci de patienter ...',
+      LOADING:
+        "Chargement des données depuis ConferenceHall, merci de patienter ..."
     },
     ERRORS: {
-      REQUIRED: 'Obligatoire',
-      SAVING: 'Echec lors de la sauvegarde.',
+      REQUIRED: "Obligatoire",
+      SAVING: "Echec lors de la sauvegarde."
     },
-    INFO: '@:EDITIONS_EDIT.INFO',
+    INFO: "@:EDITIONS_EDIT.INFO"
   },
   SPEAKER: {
-    NAME: 'Prénom & Nom',
-    ROLE: 'Rôle',
-    PROFILE_URL: 'URL de l\'image de profil',
-    COMPANY: 'Société',
-    BIO: 'Biographie',
-    GITHUB: 'GitHub',
-    TWITTER: 'Twitter',
+    NAME: "Prénom & Nom",
+    ROLE: "Rôle",
+    PROFILE_URL: "URL de l'image de profil",
+    COMPANY: "Société",
+    BIO: "Biographie",
+    GITHUB: "GitHub",
+    TWITTER: "Twitter"
   },
   EDITIONS_IMPORT: {
     LABEL: {
-      IMPORT: 'Importer depuis Conference Hall',
+      IMPORT: "Importer depuis Conference Hall"
     },
     SPEAKERS: {
-      LABEL: 'Aucun speaker | 1 speaker | {0} speakers',
+      LABEL: "Aucun speaker | 1 speaker | {0} speakers"
     },
     TALKS: {
-      LABEL: 'Aucune conférence | 1 conférence | {0} conférences',
+      LABEL: "Aucune conférence | 1 conférence | {0} conférences"
     },
-    INFO: 'Les speakers et/ou conférences déjà enregistrés dans le système seront mis à jour avec les données provenant de Conference Hall.',
-    ERROR: 'Echec lors de l\'importation, merci de réessayer ultérieurement.',
-    SUCCESS: 'Importation réussie.',
+    INFO:
+      "Les speakers et/ou conférences déjà enregistrés dans le système seront mis à jour avec les données provenant de Conference Hall.",
+    ERROR: "Echec lors de l'importation, merci de réessayer ultérieurement.",
+    SUCCESS: "Importation réussie."
   },
   TALKS_EDIT: {
     LABEL: {
-      CREATE: 'Créer une conférence',
-      UPDATE: 'Modifier une conférence',
+      CREATE: "Créer une conférence",
+      UPDATE: "Modifier une conférence"
     },
     LEVEL: {
-      BEGINNER: 'Débutant',
-      INTERMEDIATE: 'Intermédiaire',
-      ADVANCED: 'Avancée',
+      BEGINNER: "Débutant",
+      INTERMEDIATE: "Intermédiaire",
+      ADVANCED: "Avancée"
     },
     ROOM: {
-      ONE: 'Salle 1',
-      TWO: 'Salle 2',
-      THREE: 'Salle 3',
-      FOUR: 'Salle 4',
-      FIVE: 'Salle 5',
+      ONE: "Salle 1",
+      TWO: "Salle 2",
+      THREE: "Salle 3",
+      FOUR: "Salle 4",
+      FIVE: "Salle 5"
     },
-    SPEAKERS: 'Rechercher un speaker',
+    SPEAKERS: "Rechercher un speaker",
     ERRORS: {
-      REQUIRED: 'Obligatoire',
-      SAVING: 'Echec lors de la sauvegarde.',
-    },
+      REQUIRED: "Obligatoire",
+      SAVING: "Echec lors de la sauvegarde."
+    }
   },
   TALK: {
-    TITLE: 'Titre',
-    ABSTRACT: 'Description',
-    LEVEL: 'Niveau',
-    ROOM: 'Salle',
-    SPEAKERS: 'Speaker(s)',
-  },
+    TITLE: "Titre",
+    ABSTRACT: "Description",
+    LEVEL: "Niveau",
+    ROOM: "Salle",
+    SPEAKERS: "Speaker(s)"
+  }
 };
--- src/views/partners/PartnersEdit.vue
+++ src/views/partners/PartnersEdit.vue
@@ -1,175 +1,190 @@
 <template>
-    <div class="partners-edit mw-basic-layout">
-        <div class="mw-content">
-            <app-title :edition="edition"
-                       :title="isUpdatingMode ? $t('PARTNERS_EDIT.LABEL.UPDATE') : $t('PARTNERS_EDIT.LABEL.CREATE')">
-                <template slot="back">
-                    <app-back :to="back"></app-back>
-                </template>
-            </app-title>
+  <div class="partners-edit mw-basic-layout">
+    <div class="mw-content">
+      <app-title
+        :edition="edition"
+        :title="isUpdatingMode ? $t('PARTNERS_EDIT.LABEL.UPDATE') : $t('PARTNERS_EDIT.LABEL.CREATE')"
+      >
+        <template slot="back">
+          <app-back :to="back"></app-back>
+        </template>
+      </app-title>
 
-            <md-steppers :md-active-step.sync="activeStep"
-                         class="md-elevation-2"
-                         md-linear
-                         md-vertical>
-                <md-step :md-done="doneSteps.infos"
-                         :md-label="$t('PARTNERS_EDIT.INFOS')"
-                         id="infos">
-                    <md-field :class="$v.partner.name.$invalid ? 'md-invalid' : ''">
-                        <label>{{ $t('PARTNER.NAME') }}</label>
-                        <md-input autofocus
-                                  name="partnerName"
-                                  v-model.trim="partner.name">
-                        </md-input>
-                        <span class="md-error"
-                              v-if="!$v.partner.name.required">
-                            {{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}
-                        </span>
-                    </md-field>
-                    <md-field :class="$v.partner.url.$invalid ? 'md-invalid' : ''">
-                        <label>{{ $t('PARTNER.URL') }}</label>
-                        <span class="md-prefix">https://</span>
-                        <md-input name="partnerUrl"
-                                  type="url"
-                                  v-model.trim="partner.url">
-                        </md-input>
-                        <span class="md-error"
-                              v-if="!$v.partner.url.required">
-                            {{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}
-                        </span>
-                    </md-field>
-                    <app-file-uploader
-                            :disabled="$v.partner.name.$invalid"
-                            :path-on-cloud-storage="`editions/${partner.edition}/partners/${partner.name}`"
-                            v-model="partner.logoUrl">
-                    </app-file-uploader>
-                    <md-button
-                            :disabled="$v.partner.name.$invalid || $v.partner.url.$invalid || $v.partner.logoUrl.$invalid"
-                            @click="markStepAsDoneAndGo('infos', 'administrative')"
-                            class="md-primary md-raised">
-                        {{ $t('ACTIONS.CONTINUE') }}
-                    </md-button>
-                </md-step>
+      <md-steppers :md-active-step.sync="activeStep" class="md-elevation-2" md-linear md-vertical>
+        <md-step :md-done="doneSteps.infos" :md-label="$t('PARTNERS_EDIT.INFOS')" id="infos">
+          <md-field :class="$v.partner.name.$invalid ? 'md-invalid' : ''">
+            <label>{{ $t('PARTNER.NAME') }}</label>
+            <md-input autofocus name="partnerName" v-model.trim="partner.name"></md-input>
+            <span
+              class="md-error"
+              v-if="!$v.partner.name.required"
+            >{{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}</span>
+          </md-field>
+          <md-field :class="$v.partner.url.$invalid ? 'md-invalid' : ''">
+            <label>{{ $t('PARTNER.URL') }}</label>
+            <span class="md-prefix">https://</span>
+            <md-input name="partnerUrl" type="url" v-model.trim="partner.url"></md-input>
+            <span
+              class="md-error"
+              v-if="!$v.partner.url.required"
+            >{{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}</span>
+          </md-field>
 
-                <md-step :md-done="doneSteps.administrative"
-                         :md-label="$t('PARTNERS_EDIT.ADMINISTRATIVE')"
-                         id="administrative">
-                    <md-field>
-                        <label>{{ $t('PARTNER.SIRET') }}</label>
-                        <md-input name="partnerSiret"
-                                  v-model.trim="partner.siret">
-                        </md-input>
-                    </md-field>
-                    <md-field>
-                        <label>{{ $t('PARTNER.ADDRESS.ROAD') }}</label>
-                        <md-input name="partnerAddressRoad"
-                                  v-model.trim="partner.address.road">
-                        </md-input>
-                    </md-field>
-                    <md-field>
-                        <label>{{ $t('PARTNER.ADDRESS.ZIP_CODE') }}</label>
-                        <md-input name="partnerAddressZipCode"
-                                  v-model.trim="partner.address.zipCode">
-                        </md-input>
-                    </md-field>
-                    <md-field>
-                        <label>{{ $t('PARTNER.ADDRESS.TOWN') }}</label>
-                        <md-input name="partnerAddressTown"
-                                  v-model.trim="partner.address.town">
-                        </md-input>
-                    </md-field>
-                    <md-button
-                            @click="markStepAsDoneAndGo('administrative', 'contact')"
-                            class="md-primary md-raised">
-                        {{ $t('ACTIONS.CONTINUE') }}
-                    </md-button>
-                </md-step>
+          <app-file-uploader
+            :disabled="$v.partner.name.$invalid"
+            :path-on-cloud-storage="`editions/${partner.edition}/partners/${partner.name}`"
+            v-model="partner.logoUrl"
+          ></app-file-uploader>
+          <md-button
+            :disabled="$v.partner.name.$invalid || $v.partner.url.$invalid || $v.partner.logoUrl.$invalid"
+            @click="markStepAsDoneAndGo('infos', 'administrative')"
+            class="md-primary md-raised"
+          >{{ $t('ACTIONS.CONTINUE') }}</md-button>
+        </md-step>
 
-                <md-step :md-done="doneSteps.contact"
-                         :md-label="$t('PARTNERS_EDIT.CONTACT')"
-                         id="contact">
-                    <md-field>
-                        <label>{{ $t('PARTNER.CONTACT.NAME') }}</label>
-                        <md-input name="partnerContactName"
-                                  v-model.trim="partner.contact.name">
-                        </md-input>
-                    </md-field>
-                    <md-field>
-                        <label>{{ $t('PARTNER.CONTACT.FUNCTION') }}</label>
-                        <md-input name="partnerContactFunction"
-                                  v-model.trim="partner.contact.function">
-                        </md-input>
-                    </md-field>
-                    <md-button
-                            @click="markStepAsDoneAndGo('contact', 'options')"
-                            class="md-primary md-raised">
-                        {{ $t('ACTIONS.CONTINUE') }}
-                    </md-button>
-                </md-step>
-                <md-step :md-label="$t('PARTNERS_EDIT.OPTIONS')"
-                         id="options">
-                    <md-field :class="$v.partner.level.$invalid ? 'md-invalid' : ''">
-                        <label>{{ $t('PARTNER.LEVEL') }}</label>
-                        <md-select name="partnerLevel"
-                                   v-model="partner.level">
-                            <md-option :key="`level_${idx}`"
-                                       :value="level"
-                                       v-for="(level, idx) in levels">
-                                {{ $t(`PACK.${level}`) }}
-                            </md-option>
-                        </md-select>
-                        <span class="md-error"
-                              v-if="!$v.partner.level.required">
-                            {{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}
-                        </span>
-                    </md-field>
-                    <md-field :class="$v.partner.activeOn.$invalid ? 'md-invalid' : ''">
-                        <app-datetime v-model="partner.activeOn"></app-datetime>
-                        <span class="md-error"
-                              v-if="!$v.partner.activeOn.required">
-                            {{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}
-                        </span>
-                    </md-field>
-                    <md-button :disabled="$v.partner.$invalid || isSaving"
-                               @click="save"
-                               class="md-raised md-primary">
-                        {{ $t('ACTIONS.VALID') }}
-                    </md-button>
-                </md-step>
-            </md-steppers>
-        </div>
+        <md-step
+          :md-done="doneSteps.administrative"
+          :md-label="$t('PARTNERS_EDIT.ADMINISTRATIVE')"
+          id="administrative"
+        >
+          <md-field>
+            <label>{{ $t('PARTNER.SIRET') }}</label>
+            <md-input name="partnerSiret" v-model.trim="partner.siret"></md-input>
+          </md-field>
+          <md-field>
+            <label>{{ $t('PARTNER.ADDRESS.ROAD') }}</label>
+            <md-input name="partnerAddressRoad" v-model.trim="partner.address.road"></md-input>
+          </md-field>
+          <md-field>
+            <label>{{ $t('PARTNER.ADDRESS.ZIP_CODE') }}</label>
+            <md-input name="partnerAddressZipCode" v-model.trim="partner.address.zipCode"></md-input>
+          </md-field>
+          <md-field>
+            <label>{{ $t('PARTNER.ADDRESS.TOWN') }}</label>
+            <md-input name="partnerAddressTown" v-model.trim="partner.address.town"></md-input>
+          </md-field>
+          <md-button
+            @click="markStepAsDoneAndGo('administrative', 'contact')"
+            class="md-primary md-raised"
+          >{{ $t('ACTIONS.CONTINUE') }}</md-button>
+        </md-step>
+
+        <md-step :md-done="doneSteps.contact" :md-label="$t('PARTNERS_EDIT.CONTACT')" id="contact">
+          <md-field>
+            <label>{{ $t('PARTNER.CONTACT.NAME') }}</label>
+            <md-input name="partnerContactName" v-model.trim="partner.contact.name"></md-input>
+          </md-field>
+          <md-field>
+            <label>{{ $t('PARTNER.CONTACT.FUNCTION') }}</label>
+            <md-input name="partnerContactFunction" v-model.trim="partner.contact.function"></md-input>
+          </md-field>
+          <md-field>
+            <label>{{ $t('PARTNER.CONTACT.PHONE') }}</label>
+            <md-input name="partnerContactPhone" v-model.trim="partner.contact.phone"></md-input>
+          </md-field>
+          <md-field>
+            <label>{{ $t('PARTNER.CONTACT.EMAILS') }}</label>
+            <md-input
+              autocomplete="nope"
+              name="mail"
+              type="email"
+              v-model.trim="email"
+              @keypress.13="addEmail"
+            ></md-input>
+          </md-field>
+          <ul class="emails-chips">
+            <li :key="email.value" v-for="email of emails">
+              <md-chip
+                :class="email.invalid && 'md-invalid'"
+                md-deletable
+                @md-delete="deleteEmail"
+              >{{ email.value }}</md-chip>
+            </li>
+          </ul>
+          <md-button
+            @click="markStepAsDoneAndGo('contact', 'options')"
+            class="md-primary md-raised"
+          >{{ $t('ACTIONS.CONTINUE') }}</md-button>
+        </md-step>
+        <md-step :md-label="$t('PARTNERS_EDIT.OPTIONS')" id="options">
+          <md-field :class="$v.partner.level.$invalid ? 'md-invalid' : ''">
+            <label>{{ $t('PARTNER.LEVEL') }}</label>
+            <md-select name="partnerLevel" v-model="partner.level">
+              <md-option
+                :key="`level_${idx}`"
+                :value="level"
+                v-for="(level, idx) in levels"
+              >{{ $t(`PACK.${level}`) }}</md-option>
+            </md-select>
+            <span
+              class="md-error"
+              v-if="!$v.partner.level.required"
+            >{{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}</span>
+          </md-field>
+          <md-field :class="$v.partner.activeOn.$invalid ? 'md-invalid' : ''">
+            <app-datetime v-model="partner.activeOn"></app-datetime>
+            <span
+              class="md-error"
+              v-if="!$v.partner.activeOn.required"
+            >{{ $t('PARTNERS_EDIT.ERRORS.REQUIRED') }}</span>
+          </md-field>
+          <md-button
+            :disabled="$v.partner.$invalid || isSaving"
+            @click="save"
+            class="md-raised md-primary"
+          >{{ $t('ACTIONS.VALID') }}</md-button>
+        </md-step>
+      </md-steppers>
     </div>
+  </div>
 </template>
 
 <script>
-import AppBack from '@/components/app-back/AppBack.vue';
-import AppDatetime from '@/components/app-datetime/AppDatetime.vue';
-import AppFileUploader from '@/components/app-file-uploader/AppFileUploader.vue';
-import AppTitle from '@/components/app-title/AppTitle';
-import EditionsService from '@/services/EditionsService';
-import PartnersService from '@/services/PartnersService';
-import {required} from 'vuelidate/lib/validators';
+import AppBack from "@/components/app-back/AppBack.vue";
+import AppDatetime from "@/components/app-datetime/AppDatetime.vue";
+import AppFileUploader from "@/components/app-file-uploader/AppFileUploader.vue";
+import AppTitle from "@/components/app-title/AppTitle";
+import EditionsService from "@/services/EditionsService";
+import PartnersService from "@/services/PartnersService";
+import { required } from "vuelidate/lib/validators";
+
+const emailRegexp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
 
 export default {
-  name: 'PartnersEdit',
+  name: "PartnersEdit",
   components: { AppFileUploader, AppDatetime, AppBack, AppTitle },
+  computed: {
+    emails() {
+      return this.partner.emails.map(email => {
+        return {
+          invalid: !emailRegexp.test(email),
+          value: email
+        };
+      });
+    }
+  },
   data() {
     return {
       isSaving: false,
       isUpdatingMode: false,
-      activeStep: 'infos',
+      activeStep: "infos",
+      email: "",
       doneSteps: {
         first: false,
-        second: false,
+        second: false
       },
       edition: {},
       partner: {
         activeOn: new Date(),
         address: {},
         contact: {},
+        emails: []
       },
-      levels: ['GOLD', 'SILVER', 'BRONZE', 'CONTRIBUTEUR'],
-      back: { name: 'editions-dashboard', params: { editionId: this.$route.params.editionId } },
+      levels: ["GOLD", "SILVER", "BRONZE", "CONTRIBUTEUR"],
+      back: {
+        name: "editions-dashboard",
+        params: { editionId: this.$route.params.editionId }
+      }
     };
   },
   validations: {
@@ -178,55 +193,92 @@ export default {
       url: { required },
       level: { required },
       activeOn: { required },
-      logoUrl: { required },
-    },
+      logoUrl: { required }
+    }
   },
   beforeRouteEnter(to, from, next) {
-    const promises = [EditionsService.findOneForCurrentUser(to.params.editionId)];
+    const promises = [
+      EditionsService.findOneForCurrentUser(to.params.editionId)
+    ];
 
     if (to.params.partnerId !== undefined) {
       promises.push(PartnersService.findOne(to.params.partnerId));
     }
 
-    Promise
-    .all(promises)
-    .then(datas => next(vm => {
-      vm.edition = datas[0];
+    Promise.all(promises)
+      .then(datas =>
+        next(vm => {
+          vm.edition = datas[0];
 
-      if (datas.length === 2) {
-        vm.partner = datas[1];
-        vm.isUpdatingMode = true;
-      } else {
-        vm.partner.edition = to.params.editionId;
-      }
-    }))
-    .catch(() => next({ name: 'editions' }));
+          if (datas.length === 2) {
+            vm.partner = {
+              ...vm.partner,
+              ...datas[1]
+            };
+            vm.isUpdatingMode = true;
+          } else {
+            vm.partner.edition = to.params.editionId;
+          }
+        })
+      )
+      .catch(() => next({ name: "editions" }));
   },
   methods: {
+    deleteEmail(text, index) {
+      this.partner.emails.splice(index, 1);
+    },
+    addEmail() {
+      this.partner.emails.push(this.email);
+      this.email = "";
+    },
     markStepAsDoneAndGo(stepDone, stepToGo) {
       this.doneSteps[stepDone] = true;
       this.activeStep = stepToGo;
     },
     save() {
+      const partner = this.partner;
       this.isSaving = true;
       let promise;
 
+      if (this.email) {
+        partner.emails.push(this.email);
+      }
       if (this.isUpdatingMode) {
-        promise = PartnersService.update(this.$route.params.partnerId, this.partner);
+        promise = PartnersService.update(
+          this.$route.params.partnerId,
+          this.partner
+        );
       } else {
         promise = PartnersService.create(this.partner);
       }
 
       promise
-      .then(() => this.$router.push(this.back))
-      .catch(() => {
-        this.$store.commit('notification/setNotification', {
-          active: true,
-          message: this.$t('PARTNERS_EDIT.ERRORS.SAVING'),
-        });
-      })
-      .finally(() => this.isSaving = false);
-    },
-  },
+        .then(() => this.$router.push(this.back))
+        .catch(() => {
+          this.$store.commit("notification/setNotification", {
+            active: true,
+            message: this.$t("PARTNERS_EDIT.ERRORS.SAVING")
+          });
+        })
+        .finally(() => (this.isSaving = false));
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+ul.emails-chips {
+  list-style: none;
+  padding-left: 0;
+
+  li {
+    display: inline;
+    margin-right: 5px;
+  }
+}
+
+.md-theme-default.md-chip.md-invalid {
+  background: var(--md-theme-default-fieldvariant, #ff1744);
+}
+</style>
+
